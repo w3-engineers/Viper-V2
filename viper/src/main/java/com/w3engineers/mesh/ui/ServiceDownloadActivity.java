c828d016170572f6d46e93e6f6f035a60620209c
Aziz <azizul@w3engineers.com>
2021-10-22 16:37:28 +0600

    #Fix Added storage read write permission before download button press

--- viper/src/main/java/com/w3engineers/mesh/ui/ServiceDownloadActivity.java
+++ viper/src/main/java/com/w3engineers/mesh/ui/ServiceDownloadActivity.java
@@ -1,11 +1,15 @@
 package com.w3engineers.mesh.ui;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.w3engineers.mesh.R;
 import com.w3engineers.mesh.application.data.local.db.SharedPref;
@@ -19,6 +23,7 @@ import com.w3engineers.mesh.util.lib.mesh.HandlerUtil;
  */
 public class ServiceDownloadActivity extends AppCompatActivity implements ProgressListener {
     private TextView progressView;
+    private static final int REQUEST_WRITE_PERMISSION = 786;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,6 +33,23 @@ public class ServiceDownloadActivity extends AppCompatActivity implements Progre
     }
 
     public void downloadServiceApp(View view) {
+        checkPermissionAndTriggerDownload();
+    }
+
+    private void checkPermissionAndTriggerDownload() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                downloadServiceAppAfterPermissionCheck();
+            } else {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_PERMISSION);
+            }
+        } else {
+            downloadServiceAppAfterPermissionCheck();
+        }
+    }
+
+    private void downloadServiceAppAfterPermissionCheck() {
         Util.isConnected(isConnected ->
                 HandlerUtil.postForeground(() -> {
                     if (isConnected) {
@@ -37,8 +59,16 @@ public class ServiceDownloadActivity extends AppCompatActivity implements Progre
                 })
 
         );
-
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_WRITE_PERMISSION && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            checkPermissionAndTriggerDownload();
+        }
+    }
+
 
     @Override
     public void onDownloadProgress(int progress) {
